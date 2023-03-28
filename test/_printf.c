44548fde8b07b30143b3933ae6bbf63a522cb1b7
Jobs-rw <mjabush@yahoo.fr>
2023-03-28 02:15:56 -0700

    debug4

--- test/_printf.c
+++ test/_printf.c
@@ -1,110 +1,60 @@
-#include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <unistd.h>
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - produces output according to a format.
+ * @format: format string containing zero or more directives.
+ *
+ * Return: number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
-
 {
-
-int i, printed = 0, printed_chars = 0;
-
-int flags, width, precision, size, buff_ind = 0;
-
-va_list list;
-
-char buffer[BUFF_SIZE];
-
-if (format == NULL)
-
-return (-1);
-
-va_start(list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
-
-{
-
-if (format[i] != '%')
-
-{
-
-buffer[buff_ind++] = format[i];
-
-if (buff_ind == BUFF_SIZE)
-
-print_buffer(buffer, &buff_ind);
-
-/* write(1, &format[i], 1);*/
-
-printed_chars++;
-
-}
-
-else
-
-{
-
-print_buffer(buffer, &buff_ind);
-
-flags = get_flags(format, &i);
-
-width = get_width(format, &i, list);
-
-precision = get_precision(format, &i, list);
-
-size = get_size(format, &i);
-
-++i;
-
-printed = handle_print(format, &i, list, buffer,
-
-flags, width, precision, size);
-
-if (printed == -1)
-
-return (-1);
-
-printed_chars += printed;
-
-}
-
-}
-
-
-
-print_buffer(buffer, &buff_ind);
-
-
-
-va_end(list);
-
-
-
-return (printed_chars);
-
-}
-
-
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-
-void print_buffer(char buffer[], int *buff_ind)
-
-{
-
-if (*buff_ind > 0)
-
-write(1, &buffer[0], *buff_ind);
-
-
-
-*buff_ind = 0;
-
+    int count = 0;
+    va_list args;
+    char c;
+    char *s;
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    s = va_arg(args, char *);
+                    if (s == NULL)
+                        s = "(null)";
+                    while (*s)
+                    {
+                        write(1, s, 1);
+                        s++;
+                        count++;
+                    }
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    count++;
+                    break;
+                default:
+                    write(1, "%", 1);
+                    count++;
+                    write(1, &(*format), 1);
+                    count++;
+                    break;
+            }
+        }
+        else
+        {
+            write(1, &(*format), 1);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
 }
